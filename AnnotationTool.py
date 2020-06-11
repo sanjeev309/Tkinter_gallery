11d0ffe93d8cc4b8deba87b701ea2ae322e7e1cb
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2020-06-11 21:49:52 +0530

    feat(rendering default image): rendering default image in Frame

--- AnnotationTool.py
+++ AnnotationTool.py
@@ -2,32 +2,36 @@ import numpy as np
 import cv2
 import os
 
-#Install tkinter:  sudo apt install python3-tk
+# Install tkinter:  sudo apt install python3-tk
 import tkinter
 from PIL import ImageTk, Image
 
 
-
-class Interface():
+class Interface:
 
     def __init__(self):
         self.init_ui()
 
     def init_ui(self):
         self.window = tkinter.Tk()
-        self.window.minsize(400, 400)
-        tkinter.Button(self.window, text="Previous", command=self.prev_callback).grid(column=0, row=0,sticky ="NW")
-        tkinter.Button(self.window, text="Select Folder", command=self.open_folder_callback).grid(column=0, row=0,sticky ="N")
-        tkinter.Button(self.window, text="Next", command=self.next_callback).grid(column=0, row=0,sticky="NE")
+        self.window.minsize(300, 300)
+        self.window.title("Annotation tool")
+
+        tkinter.Button(self.window, text="Previous", command=self.prev_callback).grid(column=0, row=0, sticky="NW")
+        tkinter.Button(self.window, text="Select Folder", command=self.open_folder_callback).grid(column=0, row=0,
+                                                                                                  sticky="N")
+        tkinter.Button(self.window, text="Next", command=self.next_callback).grid(column=0, row=0, sticky="NE")
         # app = tkinter.Frame(self.window, bg="white")
         # # Create a label in the frame
-        default_image = self.get_default_frame()
+        self.image = self.get_default_frame()
 
-        frame = tkinter.Frame(self.window, width=300, height=300)
+        frame = tkinter.Frame(self.window, width=400, height=400)
         frame.grid()
 
-        lmain = tkinter.Label(frame)
-        lmain.image = default_image
+        lmain = tkinter.Label(frame, image=self.image)
+
+        lmain.image = self.image
+        lmain.grid()
 
         self.render_image()
 
@@ -47,12 +51,12 @@ class Interface():
         return
 
     def get_default_frame(self):
-        frame = np.zeros([300,300,3], dtype= np.uint8)
-        frame = cv2.putText(frame, "No Image to display",  (150,150),cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0))
+        frame = np.zeros([300, 300, 3], dtype=np.uint8)
+        frame = cv2.putText(frame, "No Image to display", (40, 150), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255))
         image = Image.fromarray(frame)
-        image = tkinter.PhotoImage(image)
+        # image = Image.open("sample.jpeg")
+        image = ImageTk.PhotoImage(image)
         return image
-
 
 if __name__ == "__main__":
     interface = Interface()
