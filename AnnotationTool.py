3f27a8f21c5c096d371ab4c8b48e0f1befde6d65
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2020-06-14 22:47:08 +0530

    feat(setting image to label): implemented image loading to label after resize to 300x300

--- AnnotationTool.py
+++ AnnotationTool.py
@@ -10,6 +10,7 @@ from PIL import ImageTk, Image
 
 from tkinter import filedialog
 
+
 class Interface:
 
     def __init__(self):
@@ -29,17 +30,19 @@ class Interface:
         # # Create a label in the frame
         self.image = self.get_default_frame()
 
-        frame = tkinter.Frame(self.window, width=400, height=400)
-        frame.grid()
+        self.frame = tkinter.Frame(self.window, width=400, height=400)
+        self.frame.grid()
 
-        lmain = tkinter.Label(frame, image=self.image)
+        self.lmain = tkinter.Label(self.frame, image=self.image)
+        self.lmain.image = self.image
+        self.lmain.grid()
 
-        lmain.image = self.image
-        lmain.grid()
+        self.render_image(self.image)
+        # self.window.mainloop()
 
-        self.render_image()
 
-    def render_image(self):
+    def render_image(self, image):
+        self.lmain.configure(image=image)
         self.window.mainloop()
 
     def prev_callback(self):
@@ -48,6 +51,9 @@ class Interface:
 
         print("prev called: Load:" + self.filelist[self.index])
         print(self.index)
+
+        image_path = self.filelist[self.index]
+        self.render_image(self.load_image(image_path))
         return
 
     def next_callback(self):
@@ -55,6 +61,10 @@ class Interface:
             self.index += 1
         print("next called: Load:" + self.filelist[self.index])
         print(self.index)
+
+        image_path = self.filelist[self.index]
+        self.render_image(self.load_image(image_path))
+
         return
 
     def open_folder_callback(self):
@@ -69,14 +79,26 @@ class Interface:
         print("open folder called for dir:", directory)
         return
 
-    def generate_glob(self,  directory):
+    def load_image(self, image_path):
+
+        try:
+            image = Image.open(image_path)
+        except FileNotFoundError:
+            return self.get_error_loading_frame(image_path)
+
+        image = Image.Image.resize(image, (300, 300))
+        # image = Image.fromarray(image)
+        image = ImageTk.PhotoImage(image)
+
+        return image
+
+    def generate_glob(self, directory):
         self.filelist = []
-        for name in glob.glob(os.path.join(directory,"*.jpg")):
+        for name in glob.glob(os.path.join(directory, "*.jpg")):
             print(name)
             self.filelist.append(name)
         self.index = 0
         print(self.filelist)
-
 
     def get_default_frame(self):
         frame = np.zeros([300, 300, 3], dtype=np.uint8)
@@ -85,6 +107,16 @@ class Interface:
         # image = Image.open("sample.jpeg")
         image = ImageTk.PhotoImage(image)
         return image
+
+    def get_error_loading_frame(self, path):
+        frame = np.zeros([300, 300, 3], dtype=np.uint8)
+        frame = cv2.putText(frame, "Error Loading Image:", (60, 150), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255))
+        frame = cv2.putText(frame, path, (10, 150), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
+        image = Image.fromarray(frame)
+        # image = Image.open("sample.jpeg")
+        image = ImageTk.PhotoImage(image)
+        return image
+
 
 if __name__ == "__main__":
     interface = Interface()
